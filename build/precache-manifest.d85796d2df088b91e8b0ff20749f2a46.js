e801e7ec2ff8cfc10f65644840fa78b00e71e1cb
Martin Rizo <35642313+Martin92571@users.noreply.github.com>
2019-10-23 21:12:16 -0700

    Add files via upload
    build folder

---
+++ build/precache-manifest.d85796d2df088b91e8b0ff20749f2a46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d385158554f23affa9f18197a0494d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e13a2486e5cf9ab37a5",
+    "url": "/static/css/main.eb881bec.chunk.css"
+  },
+  {
+    "revision": "13ba42f6641104229fc8",
+    "url": "/static/js/2.c18bb186.chunk.js"
+  },
+  {
+    "revision": "8e13a2486e5cf9ab37a5",
+    "url": "/static/js/main.1b56fa9d.chunk.js"
+  },
+  {
+    "revision": "3e0ba60c415c48c79c17",
+    "url": "/static/js/runtime-main.0949529b.js"
+  }
+]);
